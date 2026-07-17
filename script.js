84c3c60d1ce8c46baaf22fa30ecbbeac8e159d56
Spec-Tech <97382737+floyd11110@users.noreply.github.com>
2026-07-17 21:17:53 +0800

    Add digitalWrite tutorial topic

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ if (copyButton && inquiryText && copyStatus) {
     }
   });
 }
+
+const tutorialList = document.querySelector('#tutorials .tutorial-list');
+
+if (tutorialList && !tutorialList.querySelector('[data-topic="digital-write"]')) {
+  const digitalWriteTopic = document.createElement('div');
+  digitalWriteTopic.className = 'tutorial-item';
+  digitalWriteTopic.dataset.topic = 'digital-write';
+  digitalWriteTopic.innerHTML = `
+    <strong>03</strong>
+    <div>
+      <h3>Digital Write</h3>
+      <p>GPIO output using pinMode(OUTPUT), digitalWrite(), HIGH/LOW logic, and external LED control.</p>
+    </div>
+  `;
+
+  tutorialList.appendChild(digitalWriteTopic);
+}
